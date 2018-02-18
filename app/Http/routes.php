b26bf57f72eb0dc6c2e5c8ed2ccd5838b6c32a2e
Isaac Simon <ijsimon@sbcglobal.net>
2018-02-18 20:34:57 +0000

    played with model, controller, and routing.  Created link in php page to route created in routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('second', function () {
+    return view('second');
+});
--- resources/views/second.php
+++ resources/views/second.php
@@ -1,13 +1,10 @@
 <html><body>
-    <a href="https://storytime1.herokuapp.com/">Story Time</a>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Cloud9!';
-
-echo '<a href="/second">Second</a>'
+echo 'This is the second page';
 
 ?>
 </body>
